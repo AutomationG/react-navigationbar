0a77f7d42614e7bc6cf87fca32f917d72d24df45
AutomationG <pareek23gargi@gmail.com>
2020-07-12 03:41:32 +0530

    2 files modified

--- Router.js
+++ Router.js
@@ -3,13 +3,16 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Pages/Home.js';
 import About from './Pages/About.js';
 import Contact from './Pages/Contact.js';
+import NavigationRoute from './NavigationRoute.js';
+import navLinks from './NavigationConfig.js';
 
 const router = (
   <Router>
+   <NavigationRoute navLinks={navLinks}/>
   <Switch>
    <Home path="/" />
-   <Contact path="/Contact" />
-   <About path="/About"/>
+   <Contact path="/Pages/Contact" />
+   <About path="/Pages/About"/>
   </Switch>
   </Router>
   )
