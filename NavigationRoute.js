c98eead0fbc88e0fab489f4295b3f71d0463732c
AutomationG <pareek23gargi@gmail.com>
2020-07-12 20:16:10 +0530

    2 files modified

--- NavigationRoute.js
+++ NavigationRoute.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
 import './Scss/toolbar.scss';
 
 const NavigationRoute = (props) => {
+  const [navOpen,setNavOpen] = useState(false);
+  const [navIndex,setnavIndex] = useState(-1);
+
 return (
-<nav className="navBar">
-<ul>
+<React.Fragment>
+<div className='menu' onClick = {()=> setNavOpen(!navOpen) }>Menu</div>
+<nav className={navOpen == true ? 'active navBar' : 'comp navBar'}>
+<ul >
+<div>
 <li>
-<Link to='/'>Home</Link>
+<Link to='/'>Home<span class='icon'>H</span></Link>
 </li>
 <li>
-<Link to='/Contact'>Contact</Link>
+<Link to='/Contact'>Contact<span class='icon'>C</span></Link>
 </li>
 <li>
-<Link to='/About'>About</Link>
+<Link to='/About'>About<span class='icon'>A</span></Link>
 </li>
+</div>
 </ul>
 </nav>
+</React.Fragment>
 )
 }
 
